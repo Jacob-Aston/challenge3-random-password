f5c8f0f0419935d15e2d3d6aa233e1f00d43523a
JAston <jacob.aston1@gmail.com>
2022-10-02 23:03:25 -0600

    Added prompts and variables to .js

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  const lowerCase = {charSet: "abcdefghijklmnopqrstuvwxyz", isUsed: false};
+  const upperCase = {charSet: "ABCDEFG", isUsed: false};
+  const symbols ={charSet: "!@#$%^&*", isUsed: false};
+  const numbers = {charSet: "1234567890", isUsed: false};
+
+  let charCount = prompt("How long is your password?", "8-120")
+
+  if(confirm("Can contain lower case")) {
+lowerCase.isUsed = true;
+  }
+  if(confirm("Can contain upper case")) {
+    upperCase.isUsed = true;
+  }
+  if(confirm("Can contain symbols")) {
+    symbols.isUsed = true
+  }
+  if(confirm("Can contain numbers")) {
+    numbers.isUsed = true
+  }
+
+}
